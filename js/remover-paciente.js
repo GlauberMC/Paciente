cbcd3fffed440e81419c6577bbc409054949ccb1
Glauber <gmauch.carvalho@gmail.com>
2020-04-05 19:58:08 -0300

    Refatorando arquivo remove-paciente.js que irá remover todos os pacientes cadastrados

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -1,12 +1,10 @@
 var pacientes = document.querySelectorAll(".paciente");
 
-// passando por cada paciente da tabela
-pacientes.forEach(function(paciente){
-	// adicionando a cada paciente um escutador de duplo click
-	paciente.addEventListener("dblclick", function(){
-		// removendo paciente da tabela com o comando remove usando this
-		// this é uma palavra de contexto ligada com quem acionou o evento, 
-		// a quem o evento está atrelado. Como o evento está atrelado ao paciente, o this fará referência a ele.
-		this.remove();
-	});
+var tabela = document.querySelector("#tabela-pacientes");
+
+tabela.addEventListener("dblclick", function(event){
+	var alvoEvento = event.target;
+	var paiDoEvento = alvoEvento.parentNode; // TR = paciente = remover
+	paiDoEvento.remove()
+	// event.target.remove();
 });
