88eca9587a8c1d1aff3d2c2a844d8fefcacb5100
Glauber <gmauch.carvalho@gmail.com>
2020-04-06 10:34:17 -0300

    Adicionando animação de transição na remoção do paciente

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -5,5 +5,9 @@ var tabela = document.querySelector("#tabela-pacientes");
 tabela.addEventListener("dblclick", function(event){
 	// event.target -> será quem sofreu propriamente o evento
 	// parentNode -> seleciona o pai de um elemento
-	event.target.parentNode.remove();
+	event.target.parentNode.classList.add("fadeOut");
+	setTimeout(function(){
+		event.target.parentNode.remove();
+	},500);
+	
 });
